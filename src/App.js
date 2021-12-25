05d47fb1e6b1a5996c0e891eca9608c4d9672458
Julia Bell <juliabell021@gmai.com>
2021-12-25 11:21:18 -0500

    before first deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import m from './assets/m.jpg';
+import { works } from './assets/works.js';
+
+const url = "https://en.wikipedia.org/wiki/Lil_Peep";
+
+const Link = ({work}) => {
+return(
+  <p className='work'>
+    <a href={work.url} className='work_link'>
+      {work.title}
+    </a> 
+    &nbsp;&nbsp; ({work.date})
+  </p>
+)
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <p className="title">Madeline McFarland</p>
+      <div className="col_wrapper">
+      <div className="col one"> 
+        <img src={m} className="self" alt="maddy"/>
+        <h3 className="contact"> 
+        <a href={url} className='conact_link'>
+          <b>➀</b> LinkedIn 
+        </a> 
+        &nbsp;/ 
+        <a href={url} className='contact_link'>
+          &nbsp;<b>➁</b> Resume 
+        </a> 
+        &nbsp;/
+        <a href={url} className='contact_link'>
+          &nbsp;<b>➂</b> Email
         </a>
-      </header>
+      </h3>
+      </div>
+      <div className="col two">
+        <section>
+          <h4>About</h4>
+          <p>Lil Peep's first live performance took place on February 12, 2016 in Tucson, AZ as a member of Schemaposse. Later that year Lil Peep toured briefly with Fat Nick, Mikey The Magician, and SmokePurpp. In the spring of 2017, Lil Peep embarked on his first solo tour, performing to packed venues in three Russian cities, making his way across western Europe, and then across the United states. Soon after the tour, Lil Peep emigrated to London, where he recorded his debut studio album.</p>
+        </section>
+        <section>
+          <h4>Selected work</h4>
+          {works.map((work, i) => {
+              return <Link key={i} work={work}/>
+            })}
+        </section>
+      </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+//       <figcaption>b. 1995, gemini</figcaption>
+
+/*
+const Contact = () => {
+  
+}
+*/
---
+++ src/assets/works.js
@@ -0,0 +1,27 @@
+export const works = [
+    { 
+        title: "Come Over When You're Sober, Pt. 1",
+        date: "2017",
+        url: "https://en.wikipedia.org/wiki/Lil_Peep" 
+    },
+    { 
+        title: "Come Over When You're Sober, Pt. 1",
+        date: "2017",
+        url: "https://en.wikipedia.org/wiki/Lil_Peep" 
+    },
+    { 
+        title: "Come Over When You're Sober, Pt. 1",
+        date: "2017",
+        url: "https://en.wikipedia.org/wiki/Lil_Peep" 
+    },
+    { 
+        title: "Come Over When You're Sober, Pt. 1",
+        date: "2017",
+        url: "https://en.wikipedia.org/wiki/Lil_Peep" 
+    },
+    { 
+        title: "Come Over When You're Sober, Pt. 1",
+        date: "2017",
+        url: "https://en.wikipedia.org/wiki/Lil_Peep" 
+    }
+];
