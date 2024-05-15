917e8e1992869910f490a49be4e467a124984fba
jawblet <juliabell021@gmail.com>
2024-05-15 11:41:57 +0200

    add new work

--- src/assets/works.js
+++ src/assets/works.js
@@ -1,12 +1,17 @@
 export const works = [
     { 
-        title: "“In Session,” KGB",
+        title: "“In Session”, KGB",
         date: "2023",
         url: "https://kgbbarlit.com/fiction/session" 
     },
     { 
-        title: "“Self-Conscious,” Regarding Ingres: Fourteen Short Stories",
+        title: "“Self-Conscious”, Regarding Ingres: Fourteen Short Stories",
         date: "2023",
         url: "https://www.rizzoliusa.com/book/9780847899128/" 
+    },
+    { 
+        title: "“Forthcoming”, Gully Magazine",
+        date: "Forthcoming",
+        url: "https://gully-mag.com/" 
     }
 ];
